e5c48c5ab93f38ef662aeb481b9ee660fd770b07
General-Secretary <omar1313zedan@gmail.com>
2025-05-02 17:27:07 +0300

    Initial commit222

--- src/components/Home Comp/Describe.jsx
+++ src/components/Home Comp/Describe.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
+import { Vazirmatn } from 'next/font/google';
 import Image from 'next/image'
 import Link from 'next/link';
-import { Vazirmatn } from 'next/font/google';
-
 const vazir = Vazirmatn({
     subsets: ['arabic'],
     weight: ['400', '700']
